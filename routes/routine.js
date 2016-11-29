97263757c04df7a2d653584bce84bb7c816147c2
Sebastian Guerrero <sguerrer@andrew.cmu.edu>
2016-11-29 05:25:46 -0500

    sorta finished crud

--- routes/routine.js
+++ routes/routine.js
@@ -23,7 +23,116 @@ exports.init = function(app, passport) {
         Routine.findById(request.params.id).populate('weeks').exec(function(err, rou) {
             response.render('routines/edit', { routine: rou, user : request.user, message: request.flash('indexRoutineMessage')  });
         });
-       
+    });
+
+    app.post('/routines/edit/:id', isLoggedIn, function(request, response) {
+        let weekData = JSON.parse(request.body.weekData);
+        Routine.findById(request.params.id).populate('weeks').exec(function(err, rou) {
+            rou.name = request.body.name;
+            rou.description = request.body.description;
+
+            rou.weeks[0].sunday[0].amount = 4;
+            rou.weeks[0].save(function(err) {
+                if (err) {
+                    request.flash("newRoutineMessage","Could not save Routine");
+                    response.redirect('routines/edit/request.params.id');
+                }
+            });
+
+
+
+            for (i = 1; i < rou.weeks.length + 1; i++) { 
+                sun = rou.weeks[i - 1].sunday;
+                for (j = 0; j < sun.length; j++) { 
+                    rou.weeks[i - 1].sunday[j].order = weekData[i]["Sunday"][j].order;
+                    rou.weeks[i - 1].sunday[j].workoutID = weekData[i]["Sunday"][j].id;
+                    rou.weeks[i - 1].sunday[j].workoutName = weekData[i]["Sunday"][j].name;
+                    rou.weeks[i - 1].sunday[j].sets = weekData[i]["Sunday"][j].set;
+                    rou.weeks[i - 1].sunday[j].amount = weekData[i]["Sunday"][j].amount;
+                    rou.weeks[i - 1].sunday[j].unit = weekData[i]["Sunday"][j].unit;
+                }
+
+                mon = rou.weeks[i - 1].monday;
+                for (j = 0; j < mon.length; j++) { 
+                    rou.weeks[i - 1].monday[j].order = weekData[i]["Monday"][j].order;
+                    rou.weeks[i - 1].monday[j].workoutID = weekData[i]["Monday"][j].id;
+                    rou.weeks[i - 1].monday[j].workoutName = weekData[i]["Monday"][j].name;
+                    rou.weeks[i - 1].monday[j].sets = weekData[i]["Monday"][j].set;
+                    rou.weeks[i - 1].monday[j].amount = weekData[i]["Monday"][j].amount;
+                    rou.weeks[i - 1].monday[j].unit = weekData[i]["Monday"][j].unit;
+                }
+
+                tus = rou.weeks[i - 1].tusday;
+                for (j = 0; j < tus.length; j++) { 
+                    rou.weeks[i - 1].tusday[j].order = weekData[i]["Tusday"][j].order;
+                    rou.weeks[i - 1].tusday[j].workoutID = weekData[i]["Tusday"][j].id;
+                    rou.weeks[i - 1].tusday[j].workoutName = weekData[i]["Tusday"][j].name;
+                    rou.weeks[i - 1].tusday[j].sets = weekData[i]["Tusday"][j].set;
+                    rou.weeks[i - 1].tusday[j].amount = weekData[i]["Tusday"][j].amount;
+                    rou.weeks[i - 1].tusday[j].unit = weekData[i]["Tusday"][j].unit;
+                }
+
+                wed = rou.weeks[i - 1].wednesday;
+                for (j = 0; j < wed.length; j++) { 
+                    rou.weeks[i - 1].wednesday[j].order = weekData[i]["Wednesday"][j].order;
+                    rou.weeks[i - 1].wednesday[j].workoutID = weekData[i]["Wednesday"][j].id;
+                    rou.weeks[i - 1].wednesday[j].workoutName = weekData[i]["Wednesday"][j].name;
+                    rou.weeks[i - 1].wednesday[j].sets = weekData[i]["Wednesday"][j].set;
+                    rou.weeks[i - 1].wednesday[j].amount = weekData[i]["Wednesday"][j].amount;
+                    rou.weeks[i - 1].wednesday[j].unit = weekData[i]["Wednesday"][j].unit;
+                }
+
+                thu = rou.weeks[i - 1].thursday;
+                for (j = 0; j < thu.length; j++) { 
+                    rou.weeks[i - 1].thursday[j].order = weekData[i]["Thursday"][j].order;
+                    rou.weeks[i - 1].thursday[j].workoutID = weekData[i]["Thursday"][j].id;
+                    rou.weeks[i - 1].thursday[j].workoutName = weekData[i]["Thursday"][j].name;
+                    rou.weeks[i - 1].thursday[j].sets = weekData[i]["Thursday"][j].set;
+                    rou.weeks[i - 1].thursday[j].amount = weekData[i]["Thursday"][j].amount;
+                    rou.weeks[i - 1].thursday[j].unit = weekData[i]["Thursday"][j].unit;
+                }
+
+                fri = rou.weeks[i - 1].friday;
+                for (j = 0; j < fri.length; j++) { 
+                    rou.weeks[i - 1].friday[j].order = weekData[i]["Friday"][j].order;
+                    rou.weeks[i - 1].friday[j].workoutID = weekData[i]["Friday"][j].id;
+                    rou.weeks[i - 1].friday[j].workoutName = weekData[i]["Friday"][j].name;
+                    rou.weeks[i - 1].friday[j].sets = weekData[i]["Friday"][j].set;
+                    rou.weeks[i - 1].friday[j].amount = weekData[i]["Friday"][j].amount;
+                    rou.weeks[i - 1].friday[j].unit = weekData[i]["Friday"][j].unit;
+                }
+
+                sat = rou.weeks[i - 1].saturday;
+                for (j = 0; j < sat.length; j++) { 
+                    rou.weeks[i - 1].saturday[j].order = weekData[i]["Saturday"][j].order;
+                    rou.weeks[i - 1].saturday[j].workoutID = weekData[i]["Saturday"][j].id;
+                    rou.weeks[i - 1].saturday[j].workoutName = weekData[i]["Saturday"][j].name;
+                    rou.weeks[i - 1].saturday[j].sets = weekData[i]["Saturday"][j].set;
+                    rou.weeks[i - 1].saturday[j].amount = weekData[i]["Saturday"][j].amount;
+                    rou.weeks[i - 1].saturday[j].unit = weekData[i]["Saturday"][j].unit;
+                }
+
+                rou.weeks[i - 1].save(function(err) {
+                    if (err) {
+                        request.flash("newRoutineMessage","Could not save Routine");
+                        response.redirect('routines/edit/request.params.id');
+                    }
+                });
+            }
+
+
+
+
+
+
+            rou.save(function(err) {
+                if (err) {
+                    request.flash("newRoutineMessage","Could not save Routine");
+                    response.redirect('routines/edit/request.params.id');
+                }
+                response.redirect('/routines');
+            });
+        });
     });
 
     app.get('/api/routines/edit/:id', isLoggedIn, function(request, response) {
@@ -33,6 +142,8 @@ exports.init = function(app, passport) {
         });
        
     });
+
+    
 
     app.get('/routines/new', isLoggedIn, function(request, response) {
         response.render('routines/new', {message: request.flash('newRoutineMessage')});
@@ -103,7 +214,7 @@ exports.init = function(app, passport) {
         newRoutine.save(function(err) {
             if (err) {
                 request.flash("newRoutineMessage","Could not save Routine")
-                response.redirect('routines/new')
+                response.redirect('/routines/new')
             }
         });
         //request.flash("IndexRoutineMessage","succesfully creates routine")
